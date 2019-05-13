7583c6fcf4cc3e02055bd2d05fb2a90a897e1b46
Lexan92 <luis_grennby92@hotmail.com>
2019-05-12 23:15:39 -0600

    configuraciones extras en hibernate
    se agrego dependencias, se configuro conexion en properties, se agrego configuraciones a la configuracion del archivo de hibernate y se agrego un archivo de importacion sql de prueba

---
+++ ZATO/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Alex
+ * Created: 12/05/2019
+ */
+
+insert into PAIS (PK_PAIS,NOMBRE_PAIS) VALUES ('3','Honduras');
+
