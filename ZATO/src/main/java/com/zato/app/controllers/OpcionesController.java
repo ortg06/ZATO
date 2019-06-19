ee205f571fd76b2e4b86c089b505e14856c3d04e
Turish <ortg_10@hotmail.com>
2019-06-19 01:34:07 -0600

    opciones noguarda

--- ZATO/src/main/java/com/zato/app/controllers/OpcionesController.java
+++ ZATO/src/main/java/com/zato/app/controllers/OpcionesController.java
@@ -9,6 +9,7 @@ import com.zato.app.Servicios.IService;
 import com.zato.app.entidades.ItemPrueba;
 import com.zato.app.entidades.Opciones;
 import com.zato.app.entidades.Prueba;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,43 +17,63 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
  * @author Oscar
  */
-
 @Controller
 @RequestMapping("/items")
 @SessionAttributes("opcion")
 public class OpcionesController {
-
+    
     BigDecimal num = null;
     @Autowired
     private IService IService;
-
+    
     @RequestMapping(value = "/opciones/nuevo/{id}", method = RequestMethod.GET)
     public String crear(@PathVariable(value = "id") BigDecimal id, Map<String, Object> model) {
         Opciones opcion = new Opciones();
-
+        
         num = id;
-
+        
         model.put("item", IService.findOneItem(id));
         model.put("opcion", opcion);
-        model.put("titulo", "Nueva Opción");
-
+        model.put("titulo", "Nueva opcion para la pregunta:");
+        
         return "prueba/formO";
     }
-
+    
     @RequestMapping(value = "/opciones/formOp", method = RequestMethod.POST)
-    public String guardar(Opciones opcion, SessionStatus status) {
+    public String guardar(Opciones opcion, @RequestParam("file") MultipartFile foto, SessionStatus status) {
+        
+        if (!foto.isEmpty()) {
+            
+            try {
+                
+                byte[] content = foto.getBytes();
+                ItemPrueba p = IService.findOneItem(num);
+                opcion.setImagenOpcion(content);
+                opcion.setItemPrueba(p);
+                IService.saveOpcion(opcion);
+                status.setComplete();
+                
+            } catch (IOException e) {
+                
+                e.printStackTrace();
+            }
+            
+        }
+        
         ItemPrueba p = IService.findOneItem(num);
         opcion.setItemPrueba(p);
         IService.saveOpcion(opcion);
         status.setComplete();
-        return "redirect:/opciones/" + num;
+        return "redirect:/items/opciones/" + num;
     }
-
+    
 }
