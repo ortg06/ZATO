fce2dca44476806094b60933420d64b4f92ec0a1
zvxmiguel <jmzelayavasquez@gmail.com>
2019-06-20 20:15:58 -0600

    Revert "jm"
    This reverts commit b3f08fd95712b853e050f0bd3765fce8208ec140.

--- ZATO/src/main/java/com/zato/app/Servicios/ServiceImpl.java
+++ ZATO/src/main/java/com/zato/app/Servicios/ServiceImpl.java
@@ -42,7 +42,6 @@ import com.zato.app.dao.ILogroDao;
 import com.zato.app.dao.ICursoCapacitacionDao;
 import com.zato.app.dao.IEscritoDao;
 import com.zato.app.dao.IAptitudCvDao;
-import com.zato.app.dao.IBitacora;
 import com.zato.app.dao.IHabilidadesCvDao;
 import com.zato.app.dao.IEventoDao;
 
@@ -90,7 +89,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.zato.app.dao.IExperienciaLaboralDao;
 import com.zato.app.dao.IPruebaOfertaDao;
-import com.zato.app.entidades.Bitacora;
 import com.zato.app.entidades.PruebaOferta;
 
 /**
@@ -178,9 +176,6 @@ public class ServiceImpl implements IService {
     private IHabilidadesCvDao habilidadesCvDao;
     @Autowired
     private IEventoDao eventoDao;
-    
-    @Autowired
-    private IBitacora bitacoraDao;
     
     
     // PAISES
@@ -1227,18 +1222,6 @@ public class ServiceImpl implements IService {
     @Transactional(readOnly=true)
     public List<Evento>findCvbyEvento(Cv cv){
         return(List<Evento>) eventoDao.findCvbyEvento(cv);
-    }
-    
-     @Override
-    @Transactional
-    public List<Bitacora> findAllBitacora(){
-        return(List<Bitacora>) bitacoraDao.findAll();
-    }
-    
-    @Override
-    @Transactional
-    public Bitacora findOneBitacora (BigDecimal id){
-         return bitacoraDao.findOne(id);
     }
     
 }
